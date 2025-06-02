84299450d2d69dbd05a028784c76a2be7bfb17a5
Bektur <bekturkochorbaev64@gmail.com>
2025-06-02 23:50:16 +0600

    test

--- booking/hotel/models.py
+++ booking/hotel/models.py
@@ -55,6 +55,7 @@ class Hotel(models.Model):
     owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     created_date = models.DateField(auto_now_add=True)
     bath = models.CharField(max_length=32, null=True, blank=True)
+    car = models.CharField(max_length=32, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name_hotel}-{self.country}'
