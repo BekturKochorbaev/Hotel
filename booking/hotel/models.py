59f0c349239f286cc8da74208cb8529e2a58faed
Bektur <bekturkochorbaev64@gmail.com>
2025-06-03 00:16:58 +0600

    add five

--- booking/hotel/models.py
+++ booking/hotel/models.py
@@ -58,6 +58,7 @@ class Hotel(models.Model):
     car = models.CharField(max_length=32, null=True, blank=True)
     bike = models.CharField(max_length=32, null=True, blank=True)
     tire = models.CharField(max_length=32, null=True, blank=True)
+    five = models.CharField(max_length=32, null=True, blank=True)
 
     def __str__(self):
         return f'{self.name_hotel}-{self.country}'
